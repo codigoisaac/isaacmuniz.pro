c24bc4d2be68d9f6b4a69691103a98cf2ef46dd5
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-12-22 14:04:16 -0300

    Update MovileNav and use theme colors directly in it

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -37,6 +37,7 @@ const MobileNav = () => {
 
   return (
     <>
+      {/* "Open" button */}
       <button
         aria-label="Toggle Menu"
         onClick={onToggleNav}
@@ -46,7 +47,7 @@ const MobileNav = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          className="hover:text-primary-500 dark:hover:text-primary-400 h-8 w-8 text-gray-900 dark:text-gray-100"
+          className="h-8 w-8"
         >
           <path
             fillRule="evenodd"
@@ -60,19 +61,6 @@ const MobileNav = () => {
         <Dialog as="div" onClose={onToggleNav} unmount={false}>
           <TransitionChild
             as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            unmount={false}
-          >
-            <div className="fixed inset-0 z-60 bg-black/25" />
-          </TransitionChild>
-
-          <TransitionChild
-            as={Fragment}
             enter="transition ease-in-out duration-300 transform"
             enterFrom="translate-x-full opacity-0"
             enterTo="translate-x-0 opacity-95"
@@ -81,7 +69,7 @@ const MobileNav = () => {
             leaveTo="translate-x-full opacity-0"
             unmount={false}
           >
-            <DialogPanel className="fixed top-0 left-0 z-70 h-full w-full bg-white/95 duration-300 dark:bg-gray-950/98">
+            <DialogPanel className="fixed top-0 left-0 z-70 h-full w-full duration-300 bg-base-300">
               <nav
                 ref={navRef}
                 className="mt-8 flex h-full basis-0 flex-col items-start overflow-y-auto pt-2 pl-12 text-left"
@@ -90,7 +78,7 @@ const MobileNav = () => {
                   <Link
                     key={link.title}
                     href={link.href}
-                    className="hover:text-primary-500 dark:hover:text-primary-400 mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline-0 dark:text-gray-100 hover:underline"
+                    className="hover:text-primary-500 dark:hover:text-primary-400 mb-4 py-2 pr-4 text-2xl font-bold tracking-widest outline-0 hover:underline text-base-content"
                     onClick={onToggleNav}
                   >
                     {link.title}
@@ -99,7 +87,7 @@ const MobileNav = () => {
               </nav>
 
               <button
-                className="hover:text-primary-500 dark:hover:text-primary-400 fixed top-7 right-4 z-80 h-16 w-16 p-4 text-gray-900 dark:text-gray-100 cursor-pointer"
+                className="hover:text-primary-500 dark:hover:text-primary-400 fixed top-7 right-4 z-80 h-16 w-16 p-4 cursor-pointer text-base-content"
                 aria-label="Toggle Menu"
                 onClick={onToggleNav}
               >
