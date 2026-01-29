433306c99c87871b1edd30963318b46bdb377ddf
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-28 22:28:45 -0300

    fiz mobile nav colors

--- components/layout/MobileNav.tsx
+++ components/layout/MobileNav.tsx
@@ -63,13 +63,13 @@ export default function MobileNav() {
             as={Fragment}
             enter="transition ease-in-out duration-300 transform"
             enterFrom="translate-x-full opacity-0"
-            enterTo="translate-x-0 opacity-95"
+            enterTo="translate-x-0 opacity-80"
             leave="transition ease-in duration-200 transform"
-            leaveFrom="translate-x-0 opacity-95"
+            leaveFrom="translate-x-0 opacity-80"
             leaveTo="translate-x-full opacity-0"
             unmount={false}
           >
-            <DialogPanel className="font-transducer fixed top-0 left-0 z-70 h-full w-full duration-300 bg-base-300">
+            <DialogPanel className="font-transducer fixed top-0 left-0 z-70 h-full w-full duration-300 bg-base-100">
               <nav
                 ref={navRef}
                 className="mt-8 flex h-full basis-0 flex-col items-start overflow-y-auto pt-2 pl-12 text-left"
@@ -78,7 +78,7 @@ export default function MobileNav() {
                   <Link
                     key={link.title}
                     href={link.href}
-                    className="hover:text-primary-500 dark:hover:text-primary-400 mb-4 py-2 pr-4 text-2xl font-bold tracking-widest outline-0 hover:underline text-base-content"
+                    className="hover:text-primary mb-4 py-2 pr-4 text-2xl font-bold tracking-widest outline-0 hover:underline text-base-content"
                     onClick={onToggleNav}
                   >
                     {link.title}
@@ -86,8 +86,9 @@ export default function MobileNav() {
                 ))}
               </nav>
 
+              {/* Close Button */}
               <button
-                className="hover:text-primary-500 dark:hover:text-primary-400 fixed top-7 right-4 z-80 h-16 w-16 p-4 cursor-pointer text-base-content"
+                className="hover:text-primary fixed top-7 right-4 z-80 h-16 w-16 p-4 cursor-pointer text-base-content"
                 aria-label="Toggle Menu"
                 onClick={onToggleNav}
               >
