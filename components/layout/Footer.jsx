115e4a79883d97b63e5f2c2f0e6d789b7c2faf9f
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-30 17:44:47 -0300

    make footer more minimal

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 "use client";
 
 import {
@@ -8,15 +7,15 @@ import {
   WhatsappLogoIcon,
 } from "@phosphor-icons/react";
 
-const icon = { size: 32, weight: "duotone", className: "text-base-content" };
+const icon = { size: 28, weight: "duotone", className: "text-base-content" };
 
 export default function Footer() {
   return (
     <footer className="general-content-margins footer-paddings mt-22 w-full">
-      <div className="footer-inside-paddings border-t w-full pt-10 pb-12 flex flex-col-reverse gap-4 sm:flex-row sm:justify-between sm:place-items-center text-xs">
-        <div className="flex flex-col gap-0.5">
+      <div className="footer-inside-paddings border-t w-full pt-6 pb-9 flex flex-col-reverse gap-4 sm:flex-row sm:justify-between sm:place-items-center">
+        <div className="flex flex-col gap-0.5 text-xs">
           <div>
-            &copy; Isaac Muniz. Built with{" "}
+            &copy; Isaac Muniz. Feito com{" "}
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -36,62 +35,56 @@ export default function Footer() {
           </div>
 
           <div>im.isaac.muniz@gmail.com | +55 (19) 98195-7816</div>
-
-          <div>Your best self is eager to have you around.</div>
         </div>
 
-        <div className="flex flex-col sm:items-end gap-2">
-          <div className="text-base">Let's create good things together.</div>
+        <div className="flex gap-4">
+          <a
+            href="https://github.com/codigoisaac"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubLogoIcon
+              size={icon.size}
+              weight={icon.weight}
+              className={icon.className}
+            />
+          </a>
 
-          <div className="flex gap-4">
-            <a
-              href="https://github.com/codigoisaac"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GithubLogoIcon
-                size={icon.size}
-                weight={icon.weight}
-                className={icon.className}
-              />
-            </a>
+          <a
+            href="https://linkedin.com/in/isaac-muniz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinLogoIcon
+              size={icon.size}
+              weight={icon.weight}
+              className={icon.className}
+            />
+          </a>
 
-            <a
-              href="https://linkedin.com/in/isaac-muniz"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedinLogoIcon
-                size={icon.size}
-                weight={icon.weight}
-                className={icon.className}
-              />
-            </a>
+          <a
+            href="mailto:im.isaac.muniz@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <EnvelopeIcon
+              size={icon.size}
+              weight={icon.weight}
+              className={icon.className}
+            />
+          </a>
 
-            <a
-              href="mailto:im.isaac.muniz@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <EnvelopeIcon
-                size={icon.size}
-                weight={icon.weight}
-                className={icon.className}
-              />
-            </a>
-
-            <a
-              href="https://wa.me/5519981957816"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <WhatsappLogoIcon
-                size={icon.size}
-                weight={icon.weight}
-                className={icon.className}
-              />
-            </a>
-          </div>
+          <a
+            href="https://wa.me/5519981957816"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <WhatsappLogoIcon
+              size={icon.size}
+              weight={icon.weight}
+              className={icon.className}
+            />
+          </a>
         </div>
       </div>
     </footer>
