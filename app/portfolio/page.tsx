edbfc00785a0ea6fc407523a3f98dc78650183d5
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-02-01 22:42:29 -0300

    fine-tune layout

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -7,7 +7,7 @@ export default function PortfolioPage() {
     <>
       <PageHeader text="/portfolio" />
 
-      <div className="mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {portfolio.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
