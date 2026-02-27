b0bd28b06c0ac2b028acf3b3404de3e0a936935c
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-02-27 20:17:11 -0300

    add back button's tooltip

--- components/GoBackButton.tsx
+++ components/GoBackButton.tsx
@@ -1,5 +1,6 @@
 import { ArrowBendUpLeftIcon } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
+import { Tooltip } from "./Tooltip";
 
 interface Props {
   destination: string;
@@ -7,11 +8,13 @@ interface Props {
 
 export default function GoBackButton({ destination }: Props) {
   return (
-    <Link
-      href={`${destination}`}
-      className="hover:text-neutral hover:bg-base-300 p-1 rounded-field"
-    >
-      <ArrowBendUpLeftIcon />
-    </Link>
+    <Tooltip content="Voltar">
+      <Link
+        href={`${destination}`}
+        className="hover:text-neutral hover:bg-base-300 p-1 rounded-field"
+      >
+        <ArrowBendUpLeftIcon />
+      </Link>
+    </Tooltip>
   );
 }
--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -3,13 +3,18 @@
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 import React from "react";
+import { cn } from "@/lib/utils";
 
 export function Tooltip({
   children,
   content,
+  className,
+  arrowClassName,
 }: {
   children: React.ReactNode;
   content: string;
+  className?: string;
+  arrowClassName?: string;
 }) {
   return (
     <TooltipPrimitive.Provider delayDuration={200}>
@@ -19,10 +24,15 @@ export function Tooltip({
           <TooltipPrimitive.Content
             side="top"
             sideOffset={5}
-            className="z-100 overflow-hidden rounded-md bg-secondary px-3 py-1.5 text-xs text-secondary-content animate-in fade-in-0 zoom-in-95 font-saira font-medium tracking-wide"
+            className={cn(
+              "z-100 overflow-hidden rounded-md bg-secondary px-3 py-1.5 text-xs text-secondary-content animate-in fade-in-0 zoom-in-95 font-saira font-medium tracking-wide",
+              className,
+            )}
           >
             {content}
-            <TooltipPrimitive.Arrow className="fill-secondary" />
+            <TooltipPrimitive.Arrow
+              className={cn("fill-secondary", arrowClassName)}
+            />
           </TooltipPrimitive.Content>
         </TooltipPrimitive.Portal>
       </TooltipPrimitive.Root>
